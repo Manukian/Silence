07f1eff6f9b4e990ac1a58acd0a57dd62d6c1b63
Vagan Manukian <vaganmanukian@Vagans-MacBook-Air.local>
2023-08-29 18:37:49 +0400

    1

---
+++ script.js
@@ -0,0 +1,20 @@
+function isInViewport(element) {
+  const rect = element.getBoundingClientRect();
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+}
+
+window.addEventListener("scroll", (e) => {
+  document.querySelectorAll(".blur").forEach((block) => {
+    // block.style.filter = "blur(" + window.scrollY / 100 + "px)";
+    block.style.filter = "blur(10px)";
+    setTimeout(() => {
+      block.style.filter = "blur(0px)";
+    }, 800);
+  });
+});
